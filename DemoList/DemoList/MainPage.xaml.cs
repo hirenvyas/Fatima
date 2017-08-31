23ec88d5c753e2225c5c9452f597bc54f89a5d40
Hiren Vyas <hirenvyas047@live.in>
2017-08-31 17:40:37 +0530

    Add project files.

---
+++ DemoList/DemoList/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using DemoList.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemoList
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<User> UserList;
+        public MainPage()
+        {
+            InitializeComponent();
+            UserList = new ObservableCollection<User>()
+            {
+                new User{Id=1,Name="Hiren1" },
+                 new User{Id=2,Name="Hiren2" },
+                  new User{Id=3,Name="Hiren3" },
+                   new User{Id=4,Name="Hiren4" },
+                    new User{Id=5,Name="Hiren5" },
+            };
+            UserData.ItemsSource = UserList;
+        }
+
+        private void Add_Clicked(object sender, EventArgs e)
+        {
+            //var lastId = 1;
+            //if (UserList.Count > 1)
+            //{
+              var  lastId = UserList[UserList.Count - 1].Id;
+               
+            //}            
+            var newUser = new User();
+            newUser.Id = lastId+1;
+            newUser.Name = MyName.Text;
+            UserList.Add(newUser);
+            MyName.Text = null;
+
+        }     
+
+        private async Task Delete_Tapped_Async(object sender, EventArgs e)
+        {
+            var item = (Xamarin.Forms.Image)sender;
+            var data = (User)(((TapGestureRecognizer)item.GestureRecognizers[0]).CommandParameter);
+            if (data != null)
+            {
+                var deleteItem = await Application.Current.MainPage.DisplayAlert("Delete", "Really want to delete item", "OK", "Cancel");
+                if (deleteItem)
+                {
+                    UserList.Remove(data);
+                    
+                }
+            }
+        }
+    }
+}
